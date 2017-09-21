c8aa5cfcdea13c02c2815285d5b1a309e15d8e42
Abel Puentes Luberta <apuentes@abalt.org>
2017-09-21 20:14:04 +0200

    Added translations

---
+++ translations/es.php
@@ -0,0 +1,37 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{mc2p}prestashop>mc2p_ed180836d71bc0211c3f4ac926ba14b5'] = 'MyChoice2Pay';
+$_MODULE['<{mc2p}prestashop>mc2p_777e8bf14eb3ba3758160dc0e8650e00'] = 'Permite recibir pagos desde distintas pasarelas a la vez que ofrece la posibilidad de dividir un pago entre varias personas.';
+$_MODULE['<{mc2p}prestashop>mc2p_69a1a3ad8dd5da6db3c4da838a0cf9c7'] = '¿Estás seguro que deseas eliminar la información de MyChoice2Pay?';
+$_MODULE['<{mc2p}prestashop>mc2p_39e1cbd5b952dc0809e7cf23c4b293e2'] = 'La configuración del módulo está incompleta.';
+$_MODULE['<{mc2p}prestashop>mc2p_4347f7a90563b94d86edef5f14bb3a0a'] = 'El Key es requerido.';
+$_MODULE['<{mc2p}prestashop>mc2p_233374de68a5f0436207ec3f67536c5c'] = 'El Secret Key es requerido.';
+$_MODULE['<{mc2p}prestashop>mc2p_c888438d14855d7d96a2724ee9c306bd'] = 'Configuración actualizada';
+$_MODULE['<{mc2p}prestashop>display_form_77a684dca1390e280a052abb3c9a5fe0'] = 'Acerca de MyChoice2Pay';
+$_MODULE['<{mc2p}prestashop>display_form_f89469a46686922bee50c9f45df5defb'] = 'MyChoice2Pay permite con una sola integración recibir pagos desde distintas pasarelas a la vez que ofrece la posibilidad de que un pago sea dividido entre varias personas.';
+$_MODULE['<{mc2p}prestashop>display_form_64a00b47cd4cacf0f55daada92a30b66'] = 'mychoice2pay.com.';
+$_MODULE['<{mc2p}prestashop>display_form_6220432d4a9990aa961b3e368edaafdd'] = 'Instalación y configuración';
+$_MODULE['<{mc2p}prestashop>display_form_ff559241e76c1d7b2b31c17e28b39295'] = 'Para usar MyChoice2Pay son necesarios los siguientes pasos:';
+$_MODULE['<{mc2p}prestashop>display_form_0f98b7f230f3c91292f0de4c99e263f2'] = 'Registro';
+$_MODULE['<{mc2p}prestashop>display_form_4b603db5fcdb6e7d609c1bdb1df3f74c'] = 'Para utilizar MyChoice2Pay debe tener una cuenta de cliente. ¿Todavía no es cliente?';
+$_MODULE['<{mc2p}prestashop>display_form_15e80d88e64872dffe151e5ba783270b'] = '¡Regístrate ahora!';
+$_MODULE['<{mc2p}prestashop>display_form_b04f2c8a90cfc485fc990dc8dc4ba293'] = 'Configuración del módulo';
+$_MODULE['<{mc2p}prestashop>display_form_d5ef5873b4e981a38643192305dc6244'] = 'Escriba las credenciales de MyChoice2Pay en los siguientes campos:';
+$_MODULE['<{mc2p}prestashop>display_form_897356954c2cd3d41b221e3f24f99bba'] = 'Key';
+$_MODULE['<{mc2p}prestashop>display_form_bc224951e472e3fc4ba97383633431b6'] = 'Para deshabilitar el módulo deje este campo en blanco';
+$_MODULE['<{mc2p}prestashop>display_form_5eb6bb157528b365f84c27bb4784031b'] = 'Secret Key';
+$_MODULE['<{mc2p}prestashop>display_form_c9cc8cce247e49bae79f15173ce97354'] = 'Guardar';
+$_MODULE['<{mc2p}prestashop>display_form_6a26f548831e6a8c26bfbbd9f6ec61e0'] = 'Ayuda';
+$_MODULE['<{mc2p}prestashop>display_form_0a54158ca691945ca91a3f53d43812f4'] = 'Para instrucciones detalladas, visite nuestro';
+$_MODULE['<{mc2p}prestashop>display_form_15bbb9d0bbf25e8d2978de1168c749dc'] = 'Sitio Web';
+$_MODULE['<{mc2p}prestashop>payment_options_07f5abc2e34627f3d622bf603e112ea9'] = 'Seleccione entre diferentes opciones de pago la mejor para usted en MyChoice2Pay';
+$_MODULE['<{mc2p}prestashop>payment_options_77a81f4d23517debf4aaa0a104df6e2e'] = 'https://developer.mychoice2pay.com';
+$_MODULE['<{mc2p}prestashop>payment_options_d3da97e2d9aee5c8fbe03156ad051c99'] = 'Más';
+$_MODULE['<{mc2p}prestashop>payment_return_2e2117b7c81aa9ea6931641ea2c6499f'] = 'Su pedido en';
+$_MODULE['<{mc2p}prestashop>payment_return_75fbf512d744977d62599cc3f0ae2bb4'] = 'está completado.';
+$_MODULE['<{mc2p}prestashop>payment_return_cfa0f5d192620d230ccbf3664d9d0610'] = 'El precio total del pedido es';
+$_MODULE['<{mc2p}prestashop>payment_return_8de637e24570c1edb0357826a2ad5aea'] = 'Hemos notado un problema con su pedido. Si cree que es un error, contacte con nuestro';
+$_MODULE['<{mc2p}prestashop>payment_return_64430ad2835be8ad60c59e7d44e4b0b1'] = 'soporte al cliente';
+$_MODULE['<{mc2p}prestashop>payment_07f5abc2e34627f3d622bf603e112ea9'] = 'Seleccione entre diferentes opciones de pago la mejor para usted en MyChoice2Pay';
---
+++ translations/index.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * mc2p Module
+ *
+ * Copyright (c) 2017 MyChoice2Pay
+ *
+ * @category  Payment
+ * @author    MyChoice2Pay, <www.mychoice2pay.com>
+ * @copyright 2017, MyChoice2Pay
+ * @link      https://www.mychoice2pay.com/
+ * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ *
+ * Description:
+ *
+ * Payment module mc2p
+ *
+ * --
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to hola@mychoice2pay.com so we can send you a copy immediately.
+ */
+
+die(header('Location:../'));
+
+?>
